d25ed7d3ab3cb53534b9a1bd718af6ebb739d805
Shubham <shub.t910@gmail.com>
2023-09-14 14:45:09 +0530

    Initial Commit

---
+++ src/main/java/com/amdocs/amdocs_project/App.java
@@ -0,0 +1,13 @@
+package com.amdocs.amdocs_project;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Heldygfghgljlo World!" );
+    }
+}
