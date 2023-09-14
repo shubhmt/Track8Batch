1910434a0d78a679616da0a058ea580820118b42
Shubham <shub.t910@gmail.com>
2023-09-14 14:58:20 +0530

    Final Commit

--- src/main/java/com/amdocs/amdocs_project/App.java
+++ src/main/java/com/amdocs/amdocs_project/App.java
@@ -6,6 +6,10 @@ package com.amdocs.amdocs_project;
  */
 public class App 
 {
+	public void get()
+	{
+		System.out.println( "indicates the right file" );
+	}
     public static void main( String[] args )
     {
         System.out.println( "Heldygfghgljlo World!" );
